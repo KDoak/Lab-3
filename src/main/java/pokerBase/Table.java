42ecb0ddc7d044e9b5fb8b3c2276ca4c72e7c101
achouri <achouri@128.4.103.42>
2016-09-27 12:02:43 -0400

    q1

--- src/main/java/pokerBase/Table.java
+++ src/main/java/pokerBase/Table.java
@@ -1,5 +1,40 @@
 package pokerBase;
 
-public class Table {
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Table{
+private UUID TableID;
+private ArrayList<Player> tablePlayers = new ArrayList<Player>();
+
+public Table(UUID tableID, ArrayList<Player> tablePlayers) {
+	super();
+	TableID = tableID;
+	this.tablePlayers = tablePlayers;
+}
+
+public UUID getTableID() {
+	return TableID;
+}
+
+public void setTableID(UUID tableID) {
+	TableID = tableID;
+}
+
+public ArrayList<Player> getTablePlayers() {
+	return tablePlayers;
+}
+
+public void setTablePlayers(ArrayList<Player> tablePlayers) {
+	this.tablePlayers = tablePlayers;
+}
+
+public AddPlayerToTable(Player){
+	return tablePlayers.add(Player);
+}
 
 }
+
+
+
+
